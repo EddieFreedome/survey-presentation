3dcd77202247387dd11d05aee0fc4d5a472d7b13
EddieFreedome <eddie.freedome@tutanota.com>
2025-03-19 11:00:17 +0100

    edited users migration for login token

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,11 +14,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('login_token')->nullable()->unique();
+            
             $table->string('email')->unique()->nullable();
             $table->boolean('is_admin')->nullable();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('password')->nullable();
             $table->timestamps();
         });
 
