a70c28ac98908206e6c81d1a8cedad648b4c39a8
EddieFreedome <eddie.freedome@tutanota.com>
2025-04-09 18:39:52 +0200

    minor updates

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -29,63 +29,22 @@
         <h1 class="text-3xl pb-5 text-white">Admin dashboard</h1>
 
        <div class="table-container flex justify-center flex-col admin-card p-6">
-        {{-- Progress Grid Component --}}
-        @livewire('admin-progress-grid')
-
-        {{-- Leaderboard Component --}}
-        <div class="mt-20 admin-card p-4">
-            @livewire('leaderboard')
+            {{-- Progress Grid Component --}}
+            @livewire('admin-progress-grid')
+            {{-- Leaderboard Component --}}
+            <div class="mt-20 admin-card p-4">
+                @livewire('leaderboard')
+            </div>
+            {{-- Admin Clicker Component --}}
+            <div class="mt-8 admin-card p-4">
+                @livewire('admin-clicker')
+            </div>
         </div>
-        {{-- Admin Clicker Component --}}
-        <div class="mt-8 admin-card p-4">
-            @livewire('admin-clicker')
-        </div>
-
-        </div>
-              
-        <div class="form-container text-center pt-28">
-            {{-- <form class="text-center" method="POST" action="{{ route('logout') }}">
-                @csrf
-                <a href="{{ route('logout') }}"  onclick="event.preventDefault(); this.closest('form').submit();"
-                    class="send-btn">
-                    <strong>Log Out</strong>
-                </a>
-                
-            </form> --}}
             
-        </div>
-
-        {{-- <div class="form-container text-center pt-28">    
-    
-            <form method="POST" action="{{ route('logout') }}">
-                @csrf
-    
-                <a href=""  onclick="event.preventDefault(); alert('sei sicuro?'); this.closest('form').submit();"
-                    class="send-btn">
-                    <strong>RESET SURVEY</strong>
-                </a>
-            </form>
         
-        </div> --}}
-
-
-        {{-- <div class="form-container text-center pt-10">    
-    
-            <form method="POST" action="{{ route('logout') }}">
-                @csrf
-    
-                <a href=""  onclick="event.preventDefault(); alert('sei sicuro?'); this.closest('form').submit();"
-                    class="send-btn">
-                    <strong>START SURVEY</strong>
-                </a>
-            </form>
-        
-        </div> --}}
 
 
     </div>
-    {{-- @yield('content') --}}
-
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>    <!-- Include all compiled plugins (below), or include individual files as needed -->
 </body>
 </html>
