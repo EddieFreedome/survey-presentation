fc726e31c0b0b981c71c8105862f5bf837317428
EddieFreedome <eddie.freedome@tutanota.com>
2025-04-09 15:47:52 +0200

    added style link to login

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -13,7 +13,7 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Michroma&family=Nunito+Sans:ital,opsz,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900&display=swap" rel="stylesheet">
-
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
         <style>
             @keyframes float {
                 0% { transform: translateY(0); opacity: 0.7; }
